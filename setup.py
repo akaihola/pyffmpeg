d89fe5b2eb63de6fbe725ce0f0b524a47a7b0503
bertrand.nouvel <bertrand.nouvel@84436d8b-4227-0410-b3b9-a91164dbb45e>
2009-05-26 10:03:29 +0000

    Initial version of pyffmpegb
    git-svn-id: http://pyffmpeg.googlecode.com/svn/branches/pyffmpeg2-alpha-candidate@10 84436d8b-4227-0410-b3b9-a91164dbb45e

--- setup.py
+++ setup.py
@@ -1,32 +1,12 @@
 from distutils.core import setup
 from distutils.extension import Extension
-from Pyrex.Distutils import build_ext
+from Cython.Distutils import build_ext
 
-import sys
-if sys.platform == 'win32':
-	setup(
-	  name = "pyffmpeg",
-	  ext_modules=[ 
-	    Extension("pyffmpeg", ["pyffmpeg.pyx"],
-		define_macros=[('EMULATE_INTTYPES', '1')],
-		include_dirs=["/usr/include/ffmpeg"], 
-		library_dirs=[r"/usr/lib"], 
-		libraries = ["avutil-49","avformat-50","avcodec-51","swscale-0"])
-	    ],
-	  cmdclass = {'build_ext': build_ext}
-	)
-else:
-	setup(
-	  name = "pyffmpeg",
-	  ext_modules=[ 
-	    Extension("pyffmpeg", ["pyffmpeg.pyx"],
-		include_dirs=["/usr/include/ffmpeg"], 
-		libraries = ["avformat","avcodec","swscale"])
-	    ],
-	  cmdclass = {'build_ext': build_ext},
-	  version = "0.2.2",
-	  author = "James Evans",
-	  author_email = "jaevans@users.sf.net",
-	  url = "http://www.clark-evans.com/~milamber/pyffmpeg",
-	)
+setup(
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = [ 
+                   Extension("pyffmpegb", ["pyffmpegb.pyx"], include_dirs=["/usr/include/ffmpeg", "/usr/include/libavcodec","/usr/include/libavutil", "/usr/include/libavformat", "/usr/include/libswscale"],libraries = ["avformat","avcodec","swscale"]),
+                 ]
+)
+
 
