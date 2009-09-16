dcb8cfafcf938a4b4ba01420b2fe43e81cf5cbe6
bertrand.nouvel <bertrand.nouvel@84436d8b-4227-0410-b3b9-a91164dbb45e>
2009-09-16 10:39:39 +0000

    git-svn-id: http://pyffmpeg.googlecode.com/svn/branches/pyffmpeg2-alpha-candidate@18 84436d8b-4227-0410-b3b9-a91164dbb45e

---
+++ setup-win.py
@@ -0,0 +1,45 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+import numpy.distutils.misc_util as nd
+
+# linux
+pythonpath="/usr/lib/python2.5"
+pythonpathlib="/usr/lib/python2.5"
+# windows
+pythonpath="c:/python25"
+pythonpathlib="c:/python25/lib"
+
+ffmpegpath="../ffmpeg-0.5"
+
+#./configure --enable-shared --enable-swscale --enable-gpl --enable-memalign-hack #--enable-nonfree 
+#print dir(nd)
+
+setup(
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = [ 
+                   Extension("pyffmpegb", ["pyffmpegb.pyx"], include_dirs=[
+                      pythonpath,
+                      ffmpegpath,
+                      ffmpegpath+"/libavcodec",
+                      ffmpegpath+"/libavutil",
+                      ffmpegpath+"/libavformat",
+                      ffmpegpath+"/libswscale",
+                      "/usr/local/include/ffmpeg",
+                      "/usr/local/include/libavcodec",
+                      "/usr/local/include/libavutil",
+                      "/usr/local/include/libavformat",
+                      "/usr/local/include/libswscale",
+                      "/usr/include/ffmpeg",
+                      "/usr/include/libavcodec",
+                      "/usr/include/libavutil",
+                      "/usr/include/libavformat",
+                      "/usr/include/libswscale",
+                       ]+
+                      nd.get_numpy_include_dirs(),
+                      library_dirs=["."],
+                      libraries = ["avformat","avcodec","swscale","avutil", "wsock32"]),
+                 ]
+)
+
+
